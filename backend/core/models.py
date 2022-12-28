74859f91e5ebcc3dfe0790513d0c7d667e4a7156
zamotaev.v-a@yandex.ru <zamotaev.v-a@yandex.ru>
2022-12-29 00:27:01 +0300

    Create user model field

--- backend/core/models.py
+++ backend/core/models.py
@@ -3,20 +3,21 @@ from django.db import models
 
 
 class User(AbstractUser):
-    first_name = models.CharField(max_length=30,
+    first_name = models.CharField(max_length=150,
                                   verbose_name='Имя',
                                   null=True,
                                   blank=True)
-    last_name = models.CharField(max_length=50,
+    last_name = models.CharField(max_length=150,
                                  verbose_name='Фамилия',
                                  null=True,
                                  blank=True)
-    username = models.CharField(max_length=250,
+    username = models.CharField(max_length=150,
                                 verbose_name='Никнейм',
                                 unique=True)
     password = models.CharField(max_length=250,
                                 verbose_name='Пароль')
     email = models.EmailField(verbose_name='email address',
+                              max_length=250,
                               blank=True,
                               null=True)
 
