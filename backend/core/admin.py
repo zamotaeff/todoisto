1b4ed4f8c9b25dadcd240c3f3402973ec86e3bfd
zamotaev.v-a@yandex.ru <zamotaev.v-a@yandex.ru>
2022-12-28 03:01:21 +0300

    Add fields for user model

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+User = get_user_model()
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'username',  'username', 'email', 'id')
+    list_display_links = ('username',)
--- backend/core/models.py
+++ backend/core/models.py
@@ -1,5 +1,31 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    first_name = models.CharField(max_length=30,
+                                  verbose_name='Имя',
+                                  null=True,
+                                  blank=True)
+    last_name = models.CharField(max_length=50,
+                                 verbose_name='Фамилия',
+                                 null=True,
+                                 blank=True)
+    username = models.CharField(max_length=250,
+                                verbose_name='Никнейм',
+                                unique=True)
+    password = models.CharField(max_length=250,
+                                verbose_name='Пароль')
+    email = models.EmailField(verbose_name='email address',
+                              blank=True,
+                              null=True)
+
+    USERNAME_FIELD = 'username'
+
+    def __str__(self):
+        return f'{self.username}'
+
+    class Meta:
+        ordering = ('username',)
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
