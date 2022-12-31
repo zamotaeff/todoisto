61a770ec2ab28adb4519e5ed18d095eb2aab9706
zamotaev.v-a@yandex.ru <zamotaev.v-a@yandex.ru>
2022-12-31 10:44:31 +0300

    Update user model

--- backend/core/models.py
+++ backend/core/models.py
@@ -3,25 +3,6 @@ from django.db import models
 
 
 class User(AbstractUser):
-    first_name = models.CharField(max_length=150,
-                                  verbose_name='Имя',
-                                  null=True,
-                                  blank=True)
-    last_name = models.CharField(max_length=150,
-                                 verbose_name='Фамилия',
-                                 null=True,
-                                 blank=True)
-    username = models.CharField(max_length=150,
-                                verbose_name='Никнейм',
-                                unique=True)
-    password = models.CharField(max_length=250,
-                                verbose_name='Пароль')
-    email = models.EmailField(verbose_name='email address',
-                              max_length=250,
-                              blank=True,
-                              null=True)
-
-    USERNAME_FIELD = 'username'
 
     def __str__(self):
         return f'{self.username}'
