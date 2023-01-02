837d27c2da2cb4df763bf671eaea44a78f706e6f
zamotaev.v-a@yandex.ru <zamotaev.v-a@yandex.ru>
2023-01-02 15:30:20 +0300

    Fix serializer for login

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -41,7 +41,7 @@ class LoginSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = authenticate(
-            usernanme=validated_data['username'],
+            username=validated_data['username'],
             password=validated_data['password']
         )
         if not user:
