3344fafb89b2148b96b90b5440707046f0184178
zamotaev.v-a@yandex.ru <zamotaev.v-a@yandex.ru>
2023-01-02 15:05:25 +0300

    Remove api-auth path

--- backend/todoisto/urls.py
+++ backend/todoisto/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
+    # path('api-auth/', include('rest_framework.urls')),
     path('core/', include('core.urls')),
     path('oauth/', include('social_django.urls', namespace='social')),
 ]
