f65eb463d9549685979007f7c8f596b53f09a8be
zamotaev.v-a@yandex.ru <zamotaev.v-a@yandex.ru>
2023-01-05 20:11:23 +0300

    Fixed path for load .env file by load_dotenv

--- backend/todoisto/settings.py
+++ backend/todoisto/settings.py
@@ -18,7 +18,7 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(dotenv_path=BASE_DIR)
+load_dotenv(dotenv_path=BASE_DIR.joinpath("../.env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -29,7 +29,7 @@ SECRET_KEY = env['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env['DEBUG']
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [env['DJANGO_ALLOWED_HOSTS']]
 
 # Application definition
 
