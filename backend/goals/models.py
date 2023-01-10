ba3bab40daee3a9aa2e5b999b994d4118f86d331
zamotaev.v-a@yandex.ru <zamotaev.v-a@yandex.ru>
2023-01-10 15:31:28 +0300

    Create category model

--- backend/goals/models.py
+++ backend/goals/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+USER_MODEL = get_user_model()
+
+
+class GoalCategory(models.Model):
+    title = models.CharField(verbose_name="Название", max_length=255)
+    user = models.ForeignKey(USER_MODEL, verbose_name="Автор", on_delete=models.PROTECT)
+    is_deleted = models.BooleanField(verbose_name="Удалена", default=False)
+    created = models.DateTimeField(verbose_name="Дата создания")
+    updated = models.DateTimeField(verbose_name="Дата последнего обновления")
+
+    def save(self, *args, **kwargs):
+        if not self.id:  # Когда объект только создается, у него еще нет id
+            self.created = timezone.now()  # проставляем дату создания
+        self.updated = timezone.now()  # проставляем дату обновления
+        return super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
