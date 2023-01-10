25ef187c666883bcdaef8b05af7d81757f3c5407
zamotaev.v-a@yandex.ru <zamotaev.v-a@yandex.ru>
2023-01-10 15:31:49 +0300

    Created urls for goals app

---
+++ backend/goals/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from goals import views
+
+
+urlpatterns = [
+    path("goal_category/create", views.GoalCategoryCreateView.as_view()),
+]
--- backend/todoisto/urls.py
+++ backend/todoisto/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # path('api-auth/', include('rest_framework.urls')),
     path('core/', include('core.urls')),
+    path("goals/", include("goals.urls")),
     path('oauth/', include('social_django.urls', namespace='social')),
 ]
 
