2db52009f473837716f8cc6df334c6816661f603
zamotaev.v-a@yandex.ru <zamotaev.v-a@yandex.ru>
2023-01-11 01:37:14 +0300

    Added Status, Priority, Goal models

--- backend/goals/models.py
+++ backend/goals/models.py
@@ -2,8 +2,21 @@ from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils import timezone
 
-
 USER_MODEL = get_user_model()
+
+
+class Status(models.IntegerChoices):
+    to_do = 1, "К выполнению"
+    in_progress = 2, "В процессе"
+    done = 3, "Выполнено"
+    archived = 4, "Архив"
+
+
+class Priority(models.IntegerChoices):
+    low = 1, "Низкий"
+    medium = 2, "Средний"
+    high = 3, "Высокий"
+    critical = 4, "Критический"
 
 
 class GoalCategory(models.Model):
@@ -22,3 +35,31 @@ class GoalCategory(models.Model):
     class Meta:
         verbose_name = "Категория"
         verbose_name_plural = "Категории"
+
+
+class Goal(models.Model):
+    title = models.CharField(verbose_name="Название", max_length=255)
+    user = models.ForeignKey(USER_MODEL, verbose_name="Автор",
+                             on_delete=models.PROTECT)
+    description = models.TextField(verbose_name="Описание")
+    due_date = models.DateTimeField(verbose_name="Дата выполнения")
+    status = models.PositiveSmallIntegerField(verbose_name="Статус",
+                                              choices=Status.choices,
+                                              default=Status.to_do)
+    priority = models.PositiveSmallIntegerField(verbose_name="Приоритет",
+                                                choices=Priority.choices,
+                                                default=Priority.medium)
+    category = models.ManyToManyField(GoalCategory,
+                                      verbose_name='Категория/Категории')
+    created = models.DateTimeField(verbose_name="Дата создания")
+    updated = models.DateTimeField(verbose_name="Дата последнего обновления")
+
+    def save(self, *args, **kwargs):
+        if not self.id:  # Когда объект только создается, у него еще нет id
+            self.created = timezone.now()  # проставляем дату создания
+        self.updated = timezone.now()  # проставляем дату обновления
+        return super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = "Цель"
+        verbose_name_plural = "Цели"
