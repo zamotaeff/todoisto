8c9af7c292d42e64eb00f280797066aeddeb2702
zamotaev.v-a@yandex.ru <zamotaev.v-a@yandex.ru>
2023-01-11 01:38:00 +0300

    Added urls for goal model

--- backend/goals/urls.py
+++ backend/goals/urls.py
@@ -4,6 +4,9 @@ from goals import views
 
 
 urlpatterns = [
+    path("goal/create", views.GoalCreateView.as_view()),
+    path("goal/list", views.GoalListView.as_view()),
+    path("goal/<pk>", views.GoalView.as_view()),
     path("goal_category/create", views.GoalCategoryCreateView.as_view()),
     path("goal_category/list", views.GoalCategoryListView.as_view()),
     path("goal_category/<pk>", views.GoalCategoryView.as_view()),
