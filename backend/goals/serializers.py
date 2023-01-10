ef4979e6404a4cc9c4d76fa72717e9ec86cf0869
zamotaev.v-a@yandex.ru <zamotaev.v-a@yandex.ru>
2023-01-11 01:37:32 +0300

    Added serializers for goal model

--- backend/goals/serializers.py
+++ backend/goals/serializers.py
@@ -4,8 +4,17 @@ from goals import models
 from core.serializers import ProfileSerializer
 
 
-class GoalCreateSerializer(serializers.ModelSerializer):
+class GoalCategoryCreateSerializer(serializers.ModelSerializer):
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    def validate_category(self, value):
+        if value.is_deleted:
+            raise serializers.ValidationError("not allowed in deleted category")
+
+        if value.user != self.context["request"].user:
+            raise serializers.ValidationError("not owner of category")
+
+        return value
 
     class Meta:
         model = models.GoalCategory
@@ -18,5 +27,30 @@ class GoalCategorySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = models.GoalCategory
+        fields = "__all__"
+        read_only_fields = ("id", "created", "updated", "user")
+
+
+class GoalCreateSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    def validate_goal(self, value):
+
+        if value.user != self.context["request"].user:
+            raise serializers.ValidationError("not owner of category")
+
+        return value
+
+    class Meta:
+        model = models.Goal
+        read_only_fields = ("id", "created", "updated", "user")
+        fields = "__all__"
+
+
+class GoalSerializer(serializers.ModelSerializer):
+    user = ProfileSerializer(read_only=True)
+
+    class Meta:
+        model = models.Goal
         fields = "__all__"
         read_only_fields = ("id", "created", "updated", "user")
