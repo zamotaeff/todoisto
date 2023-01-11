a0f7e6d303daccd89df5797b8589ab85de732940
zamotaev.v-a@yandex.ru <zamotaev.v-a@yandex.ru>
2023-01-11 20:25:09 +0300

    Added DatesModelMixin

--- backend/goals/models.py
+++ backend/goals/models.py
@@ -5,6 +5,20 @@ from django.utils import timezone
 USER_MODEL = get_user_model()
 
 
+class DatesModelMixin(models.Model):
+    class Meta:
+        abstract = True  # Помечаем класс как абстрактный – для него не будет таблички в БД
+
+    created = models.DateTimeField(verbose_name="Дата создания")
+    updated = models.DateTimeField(verbose_name="Дата последнего обновления")
+
+    def save(self, *args, **kwargs):
+        if not self.id:  # Когда модель только создается – у нее нет id
+            self.created = timezone.now()
+        self.updated = timezone.now()  # Каждый раз, когда вызывается save, проставляем свежую дату обновления
+        return super().save(*args, **kwargs)
+
+
 class Status(models.IntegerChoices):
     to_do = 1, "К выполнению"
     in_progress = 2, "В процессе"
@@ -19,25 +33,17 @@ class Priority(models.IntegerChoices):
     critical = 4, "Критический"
 
 
-class GoalCategory(models.Model):
+class GoalCategory(DatesModelMixin):
     title = models.CharField(verbose_name="Название", max_length=255)
     user = models.ForeignKey(USER_MODEL, verbose_name="Автор", on_delete=models.PROTECT)
     is_deleted = models.BooleanField(verbose_name="Удалена", default=False)
-    created = models.DateTimeField(verbose_name="Дата создания")
-    updated = models.DateTimeField(verbose_name="Дата последнего обновления")
-
-    def save(self, *args, **kwargs):
-        if not self.id:  # Когда объект только создается, у него еще нет id
-            self.created = timezone.now()  # проставляем дату создания
-        self.updated = timezone.now()  # проставляем дату обновления
-        return super().save(*args, **kwargs)
 
     class Meta:
         verbose_name = "Категория"
         verbose_name_plural = "Категории"
 
 
-class Goal(models.Model):
+class Goal(DatesModelMixin):
     title = models.CharField(verbose_name="Название", max_length=255)
     user = models.ForeignKey(USER_MODEL, verbose_name="Автор",
                              on_delete=models.PROTECT)
@@ -51,14 +57,6 @@ class Goal(models.Model):
                                                 default=Priority.medium)
     category = models.ManyToManyField(GoalCategory,
                                       verbose_name='Категория/Категории')
-    created = models.DateTimeField(verbose_name="Дата создания")
-    updated = models.DateTimeField(verbose_name="Дата последнего обновления")
-
-    def save(self, *args, **kwargs):
-        if not self.id:  # Когда объект только создается, у него еще нет id
-            self.created = timezone.now()  # проставляем дату создания
-        self.updated = timezone.now()  # проставляем дату обновления
-        return super().save(*args, **kwargs)
 
     class Meta:
         verbose_name = "Цель"
