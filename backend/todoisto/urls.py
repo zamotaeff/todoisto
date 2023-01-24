69e2c10efb1b215adb9d5e6db1f2ace20227f7a9
zamotaev.v-a@yandex.ru <zamotaev.v-a@yandex.ru>
2023-01-24 12:21:02 +0300

    Fix docker-compose.yaml for tg bot

--- backend/todoisto/urls.py
+++ backend/todoisto/urls.py
@@ -3,10 +3,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('bot/', include('bot.urls')),
     path('core/', include('core.urls')),
     path('goals/', include('goals.urls')),
     path('oauth/', include('social_django.urls', namespace='social')),
     path('api-auth/', include('rest_framework.urls')),
-    path('bot/', include('bot.urls')),
-
 ]
