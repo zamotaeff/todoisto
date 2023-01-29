6f1db7c635c9c245467215f22d031481894cdda2
zamotaev.v-a@yandex.ru <zamotaev.v-a@yandex.ru>
2023-01-30 02:35:56 +0300

    Updated entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 ls -lah
+# shellcheck disable=SC2164
 cd ./backend
 python manage.py migrate --check
 status=$?
 if [[ $status != 0 ]]; then
   python manage.py migrate
 fi
+# shellcheck disable=SC2103
 cd ..
 exec "$@"
